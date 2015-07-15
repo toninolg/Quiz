3e7cd1bc29ea3b39bacddf71a4ed5c38782fd546
tonino <toninolg@gmail.com>
2015-07-15 20:45:51 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,19 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+//Pagina de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+
 router.get('/author', quizController.author);
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
