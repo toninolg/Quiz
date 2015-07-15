0094fc4a314a76fd78fe748f610253cbaa4ff12d
tonino <toninolg@gmail.com>
2015-07-15 23:43:19 +0200

    Formulario Busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,11 +17,22 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(
-	function(quizes) {
-	   	res.render('quizes/index', { quizes: quizes});
+	if ( req.query.hasOwnProperty('search') ) {
+		var str = req.query.search;
+		var search = "%" + str.replace(" ","%") + "%";
+   		models.Quiz.findAll({where: ["pregunta like ?", search]}).then(
+   		function(quizes) {
+			res.render('quizes/index', { quizes: quizes});
+   		}
+   		).catch(function(eror) {next(error);})
 	}
-	).catch(function(error) { next(error);})
+	else {
+		models.Quiz.findAll().then(
+		function(quizes) {
+		   	res.render('quizes/index', { quizes: quizes});
+		}
+		).catch(function(error) { next(error);})
+	}
 };
 
 // GET /quizes/:id
