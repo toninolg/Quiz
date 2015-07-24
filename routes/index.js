5263db524fd5ffb121efdb642ce7aee77cd574a4
linares <toninolg@gmail.com>
2015-07-24 15:50:51 +0200

    Create preguntas

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,7 @@ router.get('/author', quizController.author);
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 
 module.exports = router;
