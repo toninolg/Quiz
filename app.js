27381a98ba7c3aa2664ab858c5e90f0fce263bc2
linares <toninolg@gmail.com>
2015-07-27 14:18:44 +0200

    fixing dependency problem

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var hstore = require('pg-hstore');
 
 var routes = require('./routes/index');
 
